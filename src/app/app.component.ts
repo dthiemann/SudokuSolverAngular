04f049c66ff33aed3f3ed9d77bc4b49b558c8d58
dthiemann <dylanthiemann@gmail.com>
2021-06-15 20:40:50 -0500

    Removing fields that were used for debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,4 @@ import { Component, Input } from '@angular/core';
 export class AppComponent {
   title = 'SudokuSolver';
 
-  size_x: number = 0;
-  size_y: number = 0;
-
-  testLabel : string = "";
-
-  onSizeXChange(value : string) {
-    this.size_x = parseInt(value);
-  }
-
-  onSizeYChange(value : string) {
-    this.size_y = parseInt(value);
-  }
-
-  generate() {
-    this.testLabel = this.size_x + " x " + this.size_y;    
-  }
 }
